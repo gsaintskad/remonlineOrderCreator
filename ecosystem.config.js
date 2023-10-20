0415b6624c539d1bfea53c04872c19ad1ea84a82
bobozlo <bobckx10@gmail.com>
2023-10-20 15:09:35 +0300

    upd ignore_watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
         name: "rembot",
         script: "app.mjs",
         watch: true,
-        ignore_watch: ["node_modules", "./main.db", "\\.git","./main.db-journal"],
+        // watch: ["migrations", "remonline", "telegram"],
+        ignore_watch: ["node_modules", "./main.db", "\\.git", "*.db-journal", "./*.db-journal"],
         node_args: '-r dotenv/config',
         env: {
             ENV: "prod",
